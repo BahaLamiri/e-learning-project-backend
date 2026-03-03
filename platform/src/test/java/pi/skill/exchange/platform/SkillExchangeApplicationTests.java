6f9bacc040e6e65e95f6c919b843bddf2de3a79e
BahaLamiri <lamiribahaeddine2@gmail.com>
2026-03-03 21:28:51 +0100

    Initialisation de back-end

---
+++ platform/src/test/java/pi/skill/exchange/platform/SkillExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.skill.exchange.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
